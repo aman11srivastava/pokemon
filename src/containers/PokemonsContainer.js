a60f7394c66869bf93885f515947ce4d8a53909d
aman11srivastava <aman11srivastava@gmail.com>
2021-02-21 17:01:14 +0530

    Handled error and loading state

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -2,18 +2,37 @@ import React from "react";
 import { useQuery } from '@apollo/react-hooks'
 import { GET_POKEMONS } from '../graphql/getPokemons'
 import Pokemon from "../components/Pokemon";
+import ReactSpinner from 'react-bootstrap-spinner'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const POKEMON_COUNT = 100;
 
 export const PokemonsContainer = () => {
 
-    const { loading, data } = useQuery(GET_POKEMONS, {
+    const { loading, data, error } = useQuery(GET_POKEMONS, {
         variables: {
             first: POKEMON_COUNT
         }
     })
 
-    if(loading && !data) return <h1 style={ { textAlign: 'center', marginTop: '10rem' } }>Loading...</h1>
+    // Handling Loading state
+    if(loading && !data){
+        return (
+            <div  style={ { textAlign: 'center', marginTop: '10rem'} }>
+                <ReactSpinner type="border" size="5" />
+            </div>
+        )
+    }
+
+    // Handling error state
+    if(error && !data){
+        return(
+            <>
+                <h1 style={ { textAlign: 'center', marginTop: '13rem' } }>Unable to fetch data from the server.</h1>
+                <p style={ { textAlign: 'center'} }>Check your internet connection or try again after some time.</p>
+            </>
+        )
+    }
 
     return (
         <div className= "container">
